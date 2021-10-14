c5fa11bfea9d9b320188f6c0cd7d0bf13c40bee7
Luwam15 <80141128+Luwam15@users.noreply.github.com>
2021-10-14 13:46:44 -0500

    Lab 10 Oct 14 updated

--- src/Lab10/StandardDeviation.java
+++ src/Lab10/StandardDeviation.java
@@ -1,5 +1,6 @@
 package Lab10;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class StandardDeviation {
@@ -7,6 +8,7 @@ public class StandardDeviation {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double sum =0.0, insideCal = 0.0 , standardDeviation =0.0;
+        DecimalFormat df = new DecimalFormat("0.00");
         System.out.println("Enter numbers you want to store in the array");
         int num = input.nextInt();
         double [] arr = new double[num];
@@ -27,6 +29,6 @@ public class StandardDeviation {
             insideCal+=Math.pow(arr[i]-x,2);
         }
         standardDeviation=Math.sqrt(insideCal/num);
-        System.out.println("The Standard deviation of "+num+" numbers is: "+standardDeviation);
+        System.out.println("The Standard deviation of "+num+" numbers is: "+df.format(standardDeviation));
     }
 }
