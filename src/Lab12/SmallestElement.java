3e26f290a9bdb7bdaa09ff0c9d1aa177a47698dd
Luwam15 <80141128+Luwam15@users.noreply.github.com>
2021-10-16 15:07:39 -0500

    Lab 12 Oct 15 updated

--- src/Lab12/SmallestElement.java
+++ src/Lab12/SmallestElement.java
@@ -1,11 +1,26 @@
 package Lab12;
 
+import java.util.Scanner;
+
 public class SmallestElement {
 
     public static void main(String[] args) {
-
-            int[] num = {5, 9, 3, 1, 4, 2, 6};
-            System.out.println(smallestElement(num, 1,0));
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the Array Size you want");
+        int num = input.nextInt();
+        int[] user = new int[num];
+        for (int i = 0; i < user.length; i++) {
+            System.out.println("Enter the Element: "+(i+1));
+            user[i] = input.nextInt();
+        }
+        System.out.print("[ ");
+        for (int i = 0; i < user.length; i++) {
+            System.out.print(user[i] +"\t ");
+        }
+        System.out.print(" ]");
+        System.out.println();
+        System.out.print("The smallest element is: ");
+            System.out.println(smallestElement(user, 1,0));
 
         }
         public static int smallestElement(int[] arr, int nextVla, int index){
