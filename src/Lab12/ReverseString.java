18348b06e909bb36086e5f61382f766c8ff53a96
Luwam15 <80141128+Luwam15@users.noreply.github.com>
2021-10-17 00:14:11 -0500

    Lab 12 Oct 15 updated

--- src/Lab12/ReverseString.java
+++ src/Lab12/ReverseString.java
@@ -4,19 +4,28 @@ public class ReverseString {
 
     public static void main(String[] args) {
         String name = "liam";
-        reverseStringLab(name,0);  //Reverse for recursion
+        reverseSentence(name);  //Reverse for recursion
+        //reverseStringLab(name,0);  //Reverse for recursion
         System.out.println();
-        System.out.print(reverseString("Java")); //Reverse without recursion
+        //System.out.print(reverseString("Java")); //Reverse without recursion
 
     }
-    public static void reverseStringLab(String word, int index){
-        if (index==word.length())
-            return;
-
-        else
-            reverseStringLab(word,index+1);
-          System.out.print(word.charAt(index) +" ");
+    public static void reverseSentence(String word){
+        if (word.length() == 1) {
+            System.out.print(word);
+        }else {
+            reverseSentence(word.substring(1));
+            System.out.print(word.substring(0, 1) );
+        }
     }
+//    public static void reverseStringLab(String word, int index){
+//        if (index==word.length())
+//            return;
+//
+//        else
+//            reverseStringLab(word,index+1);
+//          System.out.print(word.charAt(index) +" ");
+//    }
 
     public static String reverseString(String word ){
         String reverse= "";
